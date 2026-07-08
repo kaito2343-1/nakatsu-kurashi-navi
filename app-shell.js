1a1ff98c8ad3c8b4699992158171a4a38bfeb452
kaito2343-1 <122045381+kaito2343-1@users.noreply.github.com>
2026-07-09 07:13:13 +0900

    Update app-shell.js

--- app-shell.js
+++ app-shell.js
@@ -71,7 +71,7 @@
     "phase3-features.js?v=1",
     "phase4-shop-status.js?v=1",
     "status-note-extra.js?v=2",
-    "phase6-business-upgrade.js?v=2"
+    "phase6-business-upgrade.js?v=3"
   ];
 
   /* 現在実行中の app-shell.js 自身のURLから、サイトのベースパスを割り出す
